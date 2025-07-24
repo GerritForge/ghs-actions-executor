023eca5bdc43e4d7cf0e4018b6c2f52cb09cc3e8
Jacek Centkowski <geminica.programs@gmail.com>
2025-07-24 15:17:54 +0200

    Move log creation and locking to a dedicated class
    This commit refactors log cration and locking into a dedicated
    `BitmapGenerationAction` class. It helps manging log from a single, well
    defined place, improving code modularity and maintainability.
    
    Related-to: #157
    Change-Id: Ia7043eca10a2efa6ab5dfa5492c793695a13df1a

---
+++ src/main/java/com/gerritforge/ghs/actions/BitmapGenerationLog.java
@@ -0,0 +1,44 @@
+// Copyright (C) 2025 GerritForge, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.ghs.actions;
+
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+class BitmapGenerationLog {
+  @FunctionalInterface
+  interface Updater {
+    void writeContent(FileChannel channel) throws IOException;
+  }
+
+  static void update(Path objectsPath, Updater updater) throws IOException {
+    Path logPath = objectsPath.resolve("pack").resolve(".ghs-packs.log");
+    try (FileChannel channel =
+            FileChannel.open(
+                logPath,
+                StandardOpenOption.CREATE,
+                StandardOpenOption.WRITE,
+                StandardOpenOption.APPEND);
+        FileLock lock = channel.lock()) {
+      updater.writeContent(channel);
+      channel.force(true);
+    }
+  }
+
+  private BitmapGenerationLog() {}
+}
